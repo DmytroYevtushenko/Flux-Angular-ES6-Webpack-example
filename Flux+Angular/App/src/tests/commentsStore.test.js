f47891750e0d4c3d9289ab299006ff1eeec62d9d
Dmitriy Yevtushenko <yevtushenkodmytrov@gmail.com>
2017-02-11 17:07:07 +0200

    add comment and notification stores tests

---
+++ Flux+Angular/App/src/tests/commentsStore.test.js
@@ -0,0 +1,32 @@
+﻿import fluxModule from 'flux-angular';
+import comments from '../comments/commentsModule';
+
+describe('Testing store', function () {
+    var notificationStore, commentsStore, dispatch, flux;
+
+    beforeEach(function () {
+        angular.mock.module(fluxModule);
+        angular.mock.module(comments);
+        inject(function ($injector) {
+            flux = $injector.get('flux');
+            notificationStore = $injector.get('NotificationStore'); 
+            commentsStore = $injector.get('CommentsStore');  
+            dispatch = flux.dispatch.bind(flux);
+        });
+    });
+
+    it('Notification store getNotifications() should be defined', function() {
+        expect(notificationStore.getNotifications()).toBeDefined();
+    });
+
+    it('Comments store getComments() should be defined', function() {
+        expect(commentsStore.getComments()).toBeDefined();
+    });
+
+    it('It should add new item', function () {
+        dispatch('COMMENT_ADD_SUCCESS', 'test');
+        expect(notificationStore.getNotifications()).toEqual(['test']);
+        expect(commentsStore.getComments()).toEqual(['test']);
+    });
+
+});
